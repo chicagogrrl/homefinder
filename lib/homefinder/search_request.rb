f93dd95a8e73ce52c8d302ba8342482d0ba9ad3a
Sara Trice <sara.trice@wowzers.com>
2014-05-10 20:02:03 -0500

    make search useful; allow for min/max range nested options

--- lib/homefinder/search_request.rb
+++ lib/homefinder/search_request.rb
@@ -3,8 +3,14 @@ module Homefinder::SearchRequest
   API_KEY = YAML.load_file( "#{Rails.root}/config/homefinder.yml" )['default']['api_key']
   base_uri "http://services.homefinder.com/listingServices/search?apikey=#{API_KEY}"
 
-  def self.simple
-    data = get("&area=60016&resultsize=1")
+  def self.retrieve options
+    options.each do |key,value|
+      if value.is_a?(Hash)
+        range = value.inject({}){|newhash,(k,v)| newhash[k] = v.blank? ? "*" : v; newhash}
+        options[key] = "#{range["min"]} TO #{range["max"]}"
+      end
+    end
+    data = get("", {query: options})
     JSON.parse(data)
   end
 
