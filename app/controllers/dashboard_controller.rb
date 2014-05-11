c55ed533c508091556f74c5fb9066b014bb02034
Sara Trice <sara.trice@wowzers.com>
2014-05-10 20:02:14 -0500

    add search form

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,8 @@
 class DashboardController < ApplicationController
   def index
   end
+
+  def search
+    @search = Homefinder::SearchRequest.retrieve(params[:search])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,12 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'dashboard#index'
 
+  resources :dashboard, only: [:index] do
+    collection do
+      post "search"
+    end
+  end
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
